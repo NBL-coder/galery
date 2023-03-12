15bcf9c0f4dba5e1bef049387a20692f49d7e198
NBL-coder <62587427+NBL-coder@users.noreply.github.com>
2023-03-12 23:30:12 +0700

    update

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
+import { UserPhoto, PhotoService } from '../services/photo.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage{
 
-  constructor() {}
+  constructor(public photoService: PhotoService,public actionSheetController: ActionSheetController) {}
 
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
+  async ngOnInit() {
+    await this.photoService.loadSaved();
+  }
+  public async showActionSheet(photo: UserPhoto, position: number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.photoService.deletePicture(photo, position);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          // Nothing to do, action sheet is automatically closed
+         }
+      }]
+    });
+    await actionSheet.present();
+  }
 }
